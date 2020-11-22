a9480f3cb0fffd9aa8ac1869800bcbe9941d09b2
xiaojie1234 <984065792@qq.com>
2020-11-22 16:06:28 +0800

    Update jd_superMarket.js

--- jd_superMarket.js
+++ jd_superMarket.js
@@ -87,7 +87,7 @@ let shareCodes = [ // IOS本地脚本用户这个列表填入你要助力的好�
     })
 async function jdSuperMarket() {
   await receiveGoldCoin();//收金币
-  //await businessCircleActivity();//商圈活动
+  await businessCircleActivity();//商圈活动
   await receiveBlueCoin();//收蓝币（小费）
   await receiveLimitProductBlueCoin();//收限时商品的蓝币
   await smtgSign();//每日签到
