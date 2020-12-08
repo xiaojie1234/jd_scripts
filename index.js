e47c5a425eeeeaf37fa95baa719ce8e2990081a4
xiaojie1234 <984065792@qq.com>
2020-12-08 23:05:20 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ exports.main_handler = async (event, context, callback) => {
       console.log(v);
       var request = require('request');
       //1.执行自己上传的js文件
-      if(v == "jd_jxstory" || v == "jd_joy_run" || v == "jd_split") {
+      if(v == "jd_jxstory" || v == "jd_joy_run" || v == "jd_split" || v == "jd_health") {
         delete require.cache[require.resolve('./'+v+'.js')];
         require('./'+v+'.js')
       } else {
